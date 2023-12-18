92e8923cdfeaba2b5306a0e86292e14b9e79ae64
mayhim182 <mayankatnitrkl@gmail.com>
2023-12-18 20:35:28 +0530

    daily code push

--- automate1.js
+++ automate1.js
@@ -1,4 +1,6 @@
 var fs = require('fs');
+var gtts = require('gtts');
+var player = require('play-sound')();
 
 calculate = () =>{
     fs.readFile('data.txt','utf-8',(err,data)=>{
@@ -16,3 +18,45 @@ calculate = () =>{
 }
 
 calculate();
+
+readAloudText = () =>{
+    fs.readFile('data1.txt','utf-8',(err,data)=>{
+        if(err){
+            throw new Error(err);
+        }
+        const gtt = new gtts(data,'en');
+        gtt.save('voice1.mp3',function (err,result){
+            if(err){
+                throw new Error(err);
+            }
+            console.log("Converted");
+            playTheConverted();
+        })
+    });
+}
+
+playTheConverted = async ()=>{
+    player.play('./voice1.mp3',(err)=>{
+        if(err){
+            throw new Error(err);
+        }
+    })
+}
+
+readAloudText();
+
+async function handleFileUpload(event){
+    const file = event.target.files.item(0);
+    const text = await file.text();
+
+    console.log(text);
+
+    if(text){
+        const fs1 = await require('fs');
+        await fs1.writeFile('./data1.txt',text,(err)=>{
+            if(err){
+                throw new Error(err);
+            }
+        })
+    }
+}
